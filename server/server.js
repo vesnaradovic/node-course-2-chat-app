7ae8730aa08434323eec3c458ca49df2f3cf9a88
vesna <vesnaradovic83@gmail.com>
2018-07-16 17:48:58 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin", 
+        text: "Welcome to the chat app",
+        createdAt: new Date().getDate()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: 'New user joined',
+        createdAt: new Date().getDate()
+    })
 
     socket.on('createMessage', (message) => {
         console.log('create message', message);
@@ -21,7 +32,12 @@ io.on('connection', (socket) => {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
+       /*  socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }) */
     });
 
     socket.on('disconnect', () => {
